8ce02850d5cabb9240b9827618d138994bfc5250
SINOVIA <somayfield@gmail.com>
2016-07-18 11:29:01 -0400

    Login Form

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,12 @@
-var portfolioApp = angular.module("portfolioApp", ['ngRoute']);
-
-
-portfolioApp.config(function($routeProvider) {
+var gentooApp = angular.module("gentooApp", ['ngRoute']);
+gentooApp.config(function($routeProvider) {
   $routeProvider
   //Consumer View Routes
+    .when('/login', {
+      templateUrl: 'templates/login.html',
+      // controller: 'ReportController'
+    })
+
     .when('/login', {
       templateUrl: 'templates/login.html',
       // controller: 'ReportController'
@@ -19,3 +22,25 @@ portfolioApp.config(function($routeProvider) {
 
     });
 });
+
+var gentooApp = angular.module('gentooApp', []);
+gentooApp.controller('FormCtrl', function ($scope, $http) {
+
+  $scope.submitForm = function() {
+      console.log("posting data....");
+      formData = $scope.form;
+      console.log(formData);
+
+      $http.post('http://7c3da702.ngrok.io/users/sign', {headers:{ 'sinovia@gentoo.com': 'password'}
+});
+};
+});
+
+//       ")
+//                   $http.post(', {email: "sinovia@gentoo.com", password: "password" })
+//                      .success(function (response) {
+//                       callback(response);
+//                     });
+//
+// };
+// });
