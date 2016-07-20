d3554f1de66a73b8716ff7c4927e4bb3a87d7936
SINOVIA <somayfield@gmail.com>
2016-07-20 13:57:16 -0400

    Controller Separated

---
+++ public/js/FormCtrl.js
@@ -0,0 +1,19 @@
+console.log("You've hacked into the Form Controller Mainframe");
+gentooApp.controller('FormCtrl', function ($scope, $http) {
+    $scope.submitForm = function() {
+        console.log("posting data....");
+        formData = $scope.form;
+        var username = $('.userlogin[name="login_username"]').val()
+        $scope.username = $('.userlogin[name="login_username"]').val()
+        $http({
+         url:  "http://58a85897.ngrok.io/" + username+ "/profile",
+         method: "GET",
+         headers: {'Authorization': username}
+     }).success(function (data, status, headers, config) {
+       alert("You will now be redirected.");
+       window.location = "#/home";
+         }).error(function (data, status, headers, config) {
+             $scope.status = status;
+         });
+    };
+ });
