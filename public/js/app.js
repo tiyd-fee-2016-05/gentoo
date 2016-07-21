d3cae4551a2981853b21d8b7d42c5b75a206b5b8
Nastassia Troyer <nntroyer@gmail.com>
2016-07-21 16:45:05 -0400

    Full merge of all files so far

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var gentooApp = angular.module("mainApp", ['ngRoute']);
-
-
+var url = "http://58a85897.ngrok.io/";
+var username = $('.userlogin[name="login_username"]').val()
 gentooApp.config(function($routeProvider) {
   $routeProvider
   //Consumer View Routes
@@ -32,65 +32,18 @@ gentooApp.config(function($routeProvider) {
           templateUrl: 'templates/add-profile.html',
           // controller: 'ReportController'
           })
+
+          .when('/friends', {
+            templateUrl: 'templates/friends.html',
+            // controller: 'ReportController'
+            })
+
+          .when('/sign-up', {
+            templateUrl: 'templates/sign-up.html',
+            // controller: 'ReportController'
+            })
       .otherwise({
         redirectTo: "/home"
 
     });
 });
-
-gentooApp.controller('FormCtrl', function ($scope, $http) {
-    $scope.submitForm = function() {
-        console.log("posting data....");
-        formData = $scope.form;
-        console.log(formData);
-        $http({
-         url: 'http://7c3da702.ngrok.io/test',
-         method: "POST",
-         data: formData,
-         headers: {'Authorization': 'sinovia@gentoo.com'}
-     }).success(function (data, status, headers, config) {
-             $scope.persons = data;
-         }).error(function (data, status, headers, config) {
-             $scope.status = status;
-         });
-    };
- });
-
- gentooApp.controller('ProfileCtrl', function ($scope, $http) {
-     $scope.addProfile = function() {
-         console.log("posting data....");
-         profileData = $scope.profile;
-         console.log(profileData);
-  $scope.message="message"
-
-         $http({
-          url: 'http://58a85897.ngrok.io/sinovia/profile',
-          method: "POST",
-          data: profileData,
-          headers: {'Authorization': 'sinovia'}
-      }).success(function (data, status, headers, config) {
-              $scope.persons = data;
-          }).error(function (data, status, headers, config) {
-              $scope.status = status;
-          });
-
-     };
-
-  });
-
-  console.log("admin.js response");
-  gentooApp.controller('LoadCtrl', function ($scope, $http) {
-
-    $http({
-      method: 'GET',
-      url: 'http://58a85897.ngrok.io/sinovia/profile/',
-          headers: {'Authorization': 'sinovia'}
-    }).success(function successCallback(response) {
-            $scope.loadedprofile= response;
-              console.log($scope.loadedprofile.user.birthday.day)
-              console.log(response)
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-  });
