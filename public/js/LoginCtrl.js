538c2725a9ebce85d0a821f1057bb66bcab7c673
Nastassia Troyer <nntroyer@gmail.com>
2016-07-23 17:34:12 -0400

    Updated menus

--- public/js/LoginCtrl.js
+++ public/js/LoginCtrl.js
@@ -49,4 +49,15 @@ console.log($rootScope.friendList[0].first_name)
 
 
     };  //End Submit Form Function
+
+
+console.log('hihihihihi');
+    $('.menu, .linee').on('click', function() {
+      $('.menu').toggleClass('over');
+      $('.linea1').toggleClass('overL1');
+      $('.linea2').toggleClass('overL2');
+      $('.linea3').toggleClass('overL3');
+      $('.voci-menu').toggleClass('overvoci');
+    });
+
  });
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,39 +1,7 @@
-// console.log('hi');
-//
-//
-// var gentooApp = angular.module('mainApp', ['ngRoute']);
-// gentooApp.controller('FriendController', function($scope) {
-//
-//   console.log('hi, again');
-//
-//   $scope.showMe = false;
-//   $scope.friendFunction = function() {
-//       $scope.hideMe = !$scope.hideMe;
-//       $scope.showMe = !$scope.showMe;
-//
-//   }
-//
-//
-//
-//
-//
-//
-// });
-
-
-//
-// $('.friend-button').click(function () {
-// $('.friend-button').css('display: none');
-// console.log('button push');
-// $('.pending-friend-button').css('display: block;');
-//
-//
-// });
-//
-// $('.pending-friend-button').click(function () {
-// $('.pending-friend-button').css('display: none');
-// console.log('button push, reverse!');
-// $('.friend-button').css('display: block;');
-//
-//
-// });
+$('.menu , .linee').on('click', function() {
+  $('.menu').toggleClass('over')
+  $('.linea1').toggleClass('overL1')
+  $('.linea2').toggleClass('overL2')
+  $('.linea3').toggleClass('overL3')
+  $('.voci-menu').toggleClass('overvoci')
+});
