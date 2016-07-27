3afbd7c5f58aa49e9c0aa57d79a337fd658b2046
SINOVIA <somayfield@gmail.com>
2016-07-27 09:51:28 -0400

    Login and Homepage populating.

--- public/js/HomeCtrl.js
+++ public/js/HomeCtrl.js
@@ -1,4 +1,4 @@
-gentooApp.controller('HomeController', ['$scope', '$http', '$location', 'User', function($scope, $http, $location, User) {
+gentooApp.controller('HomeController', ['$rootScope', '$scope', '$http', '$location', 'User', function($rootScope, $scope, $http, $location, User) {
     $scope.userToken = User.getToken();
     $scope.domain = window.location.href;
 
@@ -9,33 +9,35 @@ gentooApp.controller('HomeController', ['$scope', '$http', '$location', 'User', 
     var rootUrl= "http://6e62d5d1.ngrok.io/";
     // var rootUrl= "http://giftbox-tiy.herokuapp.com";
 
-
+console.log($scope.userToken);
+console.log($rootScope.username);
+var username = $rootScope.username
+var userToken = $scope.userToken
+    // $http({
+    //           url:  rootUrl + "/friends",
+    //        method: "GET",
+    //        headers: {'Authorization': userToken},
+    //       //  params: {formData}
+    //      }).success(function (data, status, headers, config) {
+    //               console.log("Friend Info for " + username);
+    //               console.log(data);
+    //        $rootScope.friendDetails = data
+    //       $rootScope.friendList = data.user.friends
+    //        $rootScope.friendRequests = data.user.requested_friendships
+    //               console.log("Friend Requests for " + username);
+    //               console.log("Number of Requests: " + $rootScope.friendDetails.user.requested_count);
+    //        }).error(function (data, status, headers, config) {
+    //            $scope.status = status;
+    //        });//End GET request for friends
+    //
+    //
+    //
+    //
+    //
     $http({
-              url:  rootUrl + "/" + username+ "/friends",
+              url:  rootUrl + "profile",
            method: "GET",
            headers: {'Authorization': userToken},
-          //  params: {formData}
-         }).success(function (data, status, headers, config) {
-                  console.log("Friend Info for " + username);
-                  console.log(data);
-           $rootScope.friendDetails = data
-          $rootScope.friendList = data.user.friends
-           $rootScope.friendRequests = data.user.requested_friendships
-                  console.log("Friend Requests for " + username);
-                  console.log("Number of Requests: " + $rootScope.friendDetails.user.requested_count);
-           }).error(function (data, status, headers, config) {
-               $scope.status = status;
-           });//End GET request for friends
-
-
-
-
-
-          $http({
-              url:  rootUrl+ "/" + username+ "/profile",
-           method: "GET",
-           headers: {'Authorization': username},
-           params: {formData}
        }).success(function (data, status, headers, config) {
          console.log("Profile Information for " + username);
          console.log(data);
@@ -48,15 +50,15 @@ gentooApp.controller('HomeController', ['$scope', '$http', '$location', 'User', 
            }).error(function (data, status, headers, config) {
                $scope.status = status;
            });//End GET request for profile info
-
-
-           $http({
-               url:  rootUrl + "/" + username+ "/wishlists",
-            method: "GET",
-            headers: {'Authorization': username},
+    //
+    //
+    $http({
+              url:  rootUrl + "wishlists",
+           method: "GET",
+           headers: {'Authorization': userToken},
            //  params: {formData}
           }).success(function (data, status, headers, config) {
-                   console.log("Wishlist Info for " + username);
+                   console.log("Wishlist Info for " + $rootScope.username);
                    console.log(data);
                    $rootScope.wishlistArray = data.user
                   console.log("List of Lists");
--- public/js/LoginCtrl.js
+++ public/js/LoginCtrl.js
@@ -1,75 +1,34 @@
-gentooApp.controller('LoginCtrl', function ($rootScope, $scope, $http) {
- // var rootUrl= "http://6e62d5d1.ngrok.io/";
- var rootUrl= "http://giftbox-tiy.herokuapp.com";
-
-    $scope.submitForm = function() {
-        formData = $scope.form;
-        var username = $('.userlogin[name="login_username"]').val();
-        $rootScope.username = username
-        console.log(username);
+gentooApp.controller('LoginCtrl', ['$scope', '$rootScope','$http', '$location', 'User', function($scope, $rootScope, $http, $location, User) {
+    // if (User.isLoggedIn()) {
+    //     $location.path("/");
+    // }
+    var rootUrl= "http://6e62d5d1.ngrok.io/";
+    // var rootUrl= "http://giftbox-tiy.herokuapp.com";
 
 
-
+    $scope.loginUser = function() {
+      console.log("Submitted");
         $http({
-            url:  rootUrl + "/" + username+ "/friends",
-         method: "GET",
-         headers: {'Authorization': username},
-        //  params: {formData}
-       }).success(function (data, status, headers, config) {
-                console.log("Friend Info for " + username);
-                console.log(data);
-         $rootScope.friendDetails = data
-        $rootScope.friendList = data.user.friends
-         $rootScope.friendRequests = data.user.requested_friendships
-                console.log("Friend Requests for " + username);
-                console.log("Number of Requests: " + $rootScope.friendDetails.user.requested_count);
-         }).error(function (data, status, headers, config) {
-             $scope.status = status;
-         });//End GET request for friends
+            method: "POST",
+            url:    rootUrl + "login",
+            data: {
+                email:    "sinovia@gentoo.com",
+                password: "password",
+            }
+        }).then(function(response) {
+            // TODO: store and respect expiration time??
+            User.logIn(response.data.token);
+            console.log(response.data.token);
 
+      $rootScope.username= response.data.username
+      $rootScope.token = response.data.token
 
+        }, function() {
+            alert("Something went wrong!");
+        })
+    }
 
-
-
-        $http({
-            url:  rootUrl+ "/" + username+ "/profile",
-         method: "GET",
-         headers: {'Authorization': username},
-         params: {formData}
-     }).success(function (data, status, headers, config) {
-       console.log("Profile Information for " + username);
-       console.log(data);
-           $rootScope.loadedprofile= data;
-           $rootScope.holidayList = data.user.holidays
-           $rootScope.interestList = data.user.interests
-            $rootScope.colorList = data.user.colors
-
-       window.location = "#/home";
-         }).error(function (data, status, headers, config) {
-             $scope.status = status;
-         });//End GET request for profile info
-
-
-         $http({
-             url:  rootUrl + "/" + username+ "/wishlists",
-          method: "GET",
-          headers: {'Authorization': username},
-         //  params: {formData}
-        }).success(function (data, status, headers, config) {
-                 console.log("Wishlist Info for " + username);
-                 console.log(data);
-                 $rootScope.wishlistArray = data.user
-                console.log("List of Lists");
-                 console.log($rootScope.wishlistArray);
-          }).error(function (data, status, headers, config) {
-              $scope.status = status;
-          });//End GET request for friends
-
-
-
-
-
-
-
-    };  //End Submit Form Function
- });
+    $scope.logout = function() {
+        User.logOut()
+    }
+}])
