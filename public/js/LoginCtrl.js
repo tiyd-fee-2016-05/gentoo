df3fb78b5806d8cd9a63aacc0fe6709412dccf72
Nastassia Troyer <nntroyer@gmail.com>
2016-07-27 09:06:36 -0400

    Updated

--- public/js/LoginCtrl.js
+++ public/js/LoginCtrl.js
@@ -1,34 +1,75 @@
-gentooApp.controller('LoginCtrl', ['$scope', '$http', '$location', 'User', function($scope, $http, $location, User) {
-    if (User.isLoggedIn()) {
-        $location.path("/");
-    }
-    var rootUrl= "http://6e62d5d1.ngrok.io/";
-    // var rootUrl= "http://giftbox-tiy.herokuapp.com";
+gentooApp.controller('LoginCtrl', function ($rootScope, $scope, $http) {
+ // var rootUrl= "http://6e62d5d1.ngrok.io/";
+ var rootUrl= "http://giftbox-tiy.herokuapp.com";
+
+    $scope.submitForm = function() {
+        formData = $scope.form;
+        var username = $('.userlogin[name="login_username"]').val();
+        $rootScope.username = username
+        console.log(username);
 
 
-    $scope.loginUser = function() {
-      console.log("Submitted");
+
         $http({
-            method: "POST",
-            url:    rootUrl + "/login",
-            data: {
-                email:    "sinovia@gentoo.com",
-                password: "password",
-            }
-        }).then(function(response) {
-            // TODO: store and respect expiration time??
-            User.logIn(response.data.token);
-            console.log(response.data.token);
+            url:  rootUrl + "/" + username+ "/friends",
+         method: "GET",
+         headers: {'Authorization': username},
+        //  params: {formData}
+       }).success(function (data, status, headers, config) {
+                console.log("Friend Info for " + username);
+                console.log(data);
+         $rootScope.friendDetails = data
+        $rootScope.friendList = data.user.friends
+         $rootScope.friendRequests = data.user.requested_friendships
+                console.log("Friend Requests for " + username);
+                console.log("Number of Requests: " + $rootScope.friendDetails.user.requested_count);
+         }).error(function (data, status, headers, config) {
+             $scope.status = status;
+         });//End GET request for friends
 
-            User.loggedInUser(response.data.username);
-            console.log(response.data.username);
 
-        }, function() {
-            alert("Something went wrong!");
-        })
-    }
 
-    $scope.logout = function() {
-        User.logOut()
-    }
-}])
+
+
+        $http({
+            url:  rootUrl+ "/" + username+ "/profile",
+         method: "GET",
+         headers: {'Authorization': username},
+         params: {formData}
+     }).success(function (data, status, headers, config) {
+       console.log("Profile Information for " + username);
+       console.log(data);
+           $rootScope.loadedprofile= data;
+           $rootScope.holidayList = data.user.holidays
+           $rootScope.interestList = data.user.interests
+            $rootScope.colorList = data.user.colors
+
+       window.location = "#/home";
+         }).error(function (data, status, headers, config) {
+             $scope.status = status;
+         });//End GET request for profile info
+
+
+         $http({
+             url:  rootUrl + "/" + username+ "/wishlists",
+          method: "GET",
+          headers: {'Authorization': username},
+         //  params: {formData}
+        }).success(function (data, status, headers, config) {
+                 console.log("Wishlist Info for " + username);
+                 console.log(data);
+                 $rootScope.wishlistArray = data.user
+                console.log("List of Lists");
+                 console.log($rootScope.wishlistArray);
+          }).error(function (data, status, headers, config) {
+              $scope.status = status;
+          });//End GET request for friends
+
+
+
+
+
+
+
+    };  //End Submit Form Function
+ });
