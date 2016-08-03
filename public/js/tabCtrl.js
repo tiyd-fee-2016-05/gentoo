5fecb3a908c4cce1c11b7ab69e24810b3f608452
Nastassia Troyer <nntroyer@gmail.com>
2016-08-03 15:53:08 -0400

    Updated

--- public/js/tabCtrl.js
+++ public/js/tabCtrl.js
@@ -16,105 +16,144 @@ gentooApp.controller('TabController', ['$rootScope', '$scope', '$http', '$locati
         return $scope.tab === tabNum;
     };
 
+
+// Start of old code
     // Add list items
-    $scope.FindTodos = function() {
-        console.log(usersearch);
-        var rootUrl= "https://6e62d5d1.ngrok.io/";
-        // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
-        $http({
-            url: rootUrl + usersearch + "/profile/ideabox",
-            method: "GET",
-            headers: {
-                'Authorization': userToken
-            },
-        }).success(function(data, status, headers, config) {
-            console.log(data);
-            $scope.todos = data.ideabox
+    			$scope.todos = [{
+    				text: 'iPhone case',
+    				done: false
+    			}, {
+    				text: 'Red velvet cupcakes',
+    				done: false
+    			// }, {
+    			// 	text: '',
+    			// 	done: false
+    			}];
 
-            var arr = [];
-            console.log(arr);
+    			$scope.getTotalTodos = function() {
+    				return $scope.todos.length;
+    			};
 
-            $scope.viewIdea = function() {
+    			$scope.addTodo = function() {
+    				$scope.todos.push({
+    					text: $scope.formTodoText,
+    					done: false
+    				});
+    				$scope.formTodoText = '';
+    			};
 
-                var vm = this.todo;
-                console.log(vm);
-                arr.push(vm.id);
-                console.log(arr);
-              };
+    			$scope.clearCompleted = function() {
+    				$scope.todos = _.filter($scope.todos, function(todo) {
+    					return !todo.done;
+    				});
+    			};
 
-              $scope.clearCompleted = function() {
-
-                var tobeDeleted = arr
-
-                console.log("Delete these please:" + tobeDeleted);
-                var rootUrl= "https://6e62d5d1.ngrok.io/";
-                // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
-                $scope.userToken = User.getToken();
-                var userToken = $scope.userToken
-                var usersearch = $scope.friendtoLoad
-                console.log(userToken)
-               console.log(usersearch);
-
-               $http({
-                   url: rootUrl + usersearch + "/profile/ideabox",
-                   method: "DELETE",
-                   headers: {
-                       'Authorization': userToken
-                   },
-                  params: {
-                       cleared_ideas: tobeDeleted
-                   }
-               }).success(function(data, status, headers, config) {
-                       console.log(data);
-                       $scope.todos = data.ideabox
-                       console.log(data);
+    	}]);
+    // End of old code
 
 
+    // Comment this back in!
+
+    // Add list items
+    // $scope.FindTodos = function() {
+    //     console.log(usersearch);
+    //     var rootUrl= "https://6e62d5d1.ngrok.io/";
+    //     // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
+    //     $http({
+    //         url: rootUrl + usersearch + "/profile/ideabox",
+    //         method: "GET",
+    //         headers: {
+    //             'Authorization': userToken
+    //         },
+    //     }).success(function(data, status, headers, config) {
+    //         console.log(data);
+    //         $scope.todos = data.ideabox
+    //
+    //         var arr = [];
+    //         console.log(arr);
+    //
+    //         $scope.viewIdea = function() {
+    //
+    //             var vm = this.todo;
+    //             console.log(vm);
+    //             arr.push(vm.id);
+    //             console.log(arr);
+    //           };
+    //
+
+    //           $scope.clearCompleted = function() {
+    //
+    //             var tobeDeleted = arr
+    //
+    //             console.log("Delete these please:" + tobeDeleted);
+    //             var rootUrl= "https://6e62d5d1.ngrok.io/";
+    //             // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
+    //             $scope.userToken = User.getToken();
+    //             var userToken = $scope.userToken
+    //             var usersearch = $scope.friendtoLoad
+    //             console.log(userToken)
+    //            console.log(usersearch);
+    //
+    //            $http({
+    //                url: rootUrl + usersearch + "/profile/ideabox",
+    //                method: "DELETE",
+    //                headers: {
+    //                    'Authorization': userToken
+    //                },
+    //               params: {
+    //                    cleared_ideas: tobeDeleted
+    //                }
+    //            }).success(function(data, status, headers, config) {
+    //                    console.log(data);
+    //                    $scope.todos = data.ideabox
+    //                    console.log(data);
+    //
+    //
+    //
+    //
+    //                    }).error(function(data, status, headers, config) {
+    //                    $scope.status = status;
+    //                });
+    //
+    //           };
+    //
+    //
+    //
+    //         console.log(data);
+    //     }).error(function(data, status, headers, config) {
+    //         $scope.status = status;
+    //     });
+    // }
+
+    // $scope.AddIdea = function() {
+    //     var rootUrl= "https://6e62d5d1.ngrok.io/";
+    //     // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
+    //     console.log("You had an idea!");
+    //     $http({
+    //         url: rootUrl + usersearch + "/profile/ideabox",
+    //         method: "POST",
+    //         headers: {
+    //             'Authorization': userToken
+    //         },
+    //         data: {
+    //             text: $('#ideaboxText').val(),
+    //         }
+    //     }).success(function(data, status, headers, config) {
+    //             console.log(data);
+    //             $scope.todos = data.ideabox
+    //             console.log(data);
+    //             console.log(response);
+    //             console.log(response.data);
+    //
+    //
+    //             }).error(function(data, status, headers, config) {
+    //             $scope.status = status;
+    //         });
+    //     }
 
 
-                       }).error(function(data, status, headers, config) {
-                       $scope.status = status;
-                   });
 
-              };
-
-
-
-            console.log(data);
-        }).error(function(data, status, headers, config) {
-            $scope.status = status;
-        });
-    }
-
-    $scope.AddIdea = function() {
-        var rootUrl= "https://6e62d5d1.ngrok.io/";
-        // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
-        console.log("You had an idea!");
-        $http({
-            url: rootUrl + usersearch + "/profile/ideabox",
-            method: "POST",
-            headers: {
-                'Authorization': userToken
-            },
-            data: {
-                text: $('#ideaboxText').val(),
-            }
-        }).success(function(data, status, headers, config) {
-                console.log(data);
-                $scope.todos = data.ideabox
-                console.log(data);
-                console.log(response);
-                console.log(response.data);
-
-
-                }).error(function(data, status, headers, config) {
-                $scope.status = status;
-            });
-        }
-
-
-
-        var rootUrl= "https://6e62d5d1.ngrok.io/";
+        // var rootUrl= "https://6e62d5d1.ngrok.io/";
         // var rootUrl = "https://giftbox-tiy.herokuapp.com/";
 
         // $http({
@@ -131,24 +170,24 @@ gentooApp.controller('TabController', ['$rootScope', '$scope', '$http', '$locati
         // 				});
 
 
-    $scope.getTotalTodos = function() {
-        return $scope.todos.length;
-    };
-
-    $scope.addTodo = function() {
-        $scope.todos.push({
-            text: $scope.formTodoText,
-            done: false
-        });
-        $scope.formTodoText = '';
-
-    };
+    // $scope.getTotalTodos = function() {
+    //     return $scope.todos.length;
+    // };
+    //
+    // $scope.addTodo = function() {
+    //     $scope.todos.push({
+    //         text: $scope.formTodoText,
+    //         done: false
+    //     });
+    //     $scope.formTodoText = '';
+    //
+    // };
 
 
     // Needs to clear selected items from list
 
 
-}]);
+// }]);
 
 // tabCtrl.js
 gentooApp.controller('TabController2', ['$scope', function($scope) {
